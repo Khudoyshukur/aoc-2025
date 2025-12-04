a0c1a927ae7f0eaccfe059283755cf137d1fae18
kjuraev <kjuraev.001@gmail.com>
2025-12-04 10:29:09 +0500

    Complete day4

--- src/aoc/day4/Day4.kt
+++ src/aoc/day4/Day4.kt
@@ -5,10 +5,42 @@ import java.io.FileReader
 
 fun main() {
 
-    val file = File("src/aoc/day4/input1.txt")
+    val file = File("src/aoc/day4/input.txt")
     file.createNewFile()
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
-    println(input)
+    val lines = input.split("\n")
+    val matrix = Array(lines.size) { CharArray(lines.first().length) }
+
+    for (i in lines.indices) {
+        for (j in lines.first().indices) {
+            matrix[i][j] = lines[i][j]
+        }
+    }
+
+    var count = 0
+    val col = arrayOf(1, 1, 1, -1, -1, -1, 0, 0)
+    val row = arrayOf(0, -1, 1, 0, -1, 1, 1, -1)
+    for (i in matrix.indices) {
+        for (j in matrix[0].indices) {
+            if (matrix[i][j] != '@') continue
+            var adjCount = 0
+
+            for (k in 0 until 8) {
+                val newI = i + row[k]
+                val newJ = j + col[k]
+
+                if (matrix.getOrNull(newI)?.getOrNull(newJ) == '@') {
+                    adjCount++
+                }
+            }
+
+            if (adjCount < 4) {
+                count++
+            }
+        }
+    }
+
+    println(count)
 }
--- src/aoc/day4/Day4_2.kt
+++ src/aoc/day4/Day4_2.kt
@@ -2,9 +2,10 @@ package aoc.day4
 
 import java.io.File
 import java.io.FileReader
+import java.util.LinkedList
+import java.util.Queue
 
-
-val memo = hashMapOf<Pair<Int, Int>, String>()
+data class Index(val i: Int, val j: Int)
 
 fun main() {
 
@@ -13,4 +14,76 @@ fun main() {
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
+    val lines = input.split("\n")
+    val matrix = Array(lines.size) { CharArray(lines.first().length) }
+
+    println(matrix.size)
+    println(matrix.first().size)
+
+    for (i in lines.indices) {
+        for (j in lines.first().indices) {
+            matrix[i][j] = lines[i][j]
+        }
+    }
+
+    val adjList = hashMapOf<Index, MutableSet<Index>>()
+
+    val col = arrayOf(1, 1, 1, -1, -1, -1, 0, 0)
+    val row = arrayOf(0, -1, 1, 0, -1, 1, 1, -1)
+    for (i in matrix.indices) {
+        for (j in matrix[0].indices) {
+            if (matrix[i][j] != '@') continue
+            val index = Index(i, j)
+            adjList[index] = mutableSetOf<Index>()
+
+            for (k in 0 until 8) {
+                val newI = i + row[k]
+                val newJ = j + col[k]
+
+                if (matrix.getOrNull(newI)?.getOrNull(newJ) == '@') {
+                    val curr = adjList.getOrDefault(
+                        index,  mutableSetOf<Index>()
+                    )
+                    curr.add(Index(newI, newJ))
+
+                    adjList[index] = curr
+                }
+            }
+        }
+    }
+
+    var count = 0
+    val queue: Queue<Index> = LinkedList()
+
+    val added = mutableSetOf<Index>()
+    for (key in adjList.keys) {
+        if (adjList[key]!!.size < 4) {
+            queue.add(key)
+            added.add(key)
+        }
+    }
+
+//    println(adjList)
+//    println(queue)
+
+    while (queue.isNotEmpty()) {
+        val rmIndex = queue.remove()
+        count++
+
+        for (adj in adjList[rmIndex].orEmpty()) {
+            val curr = adjList[adj] ?: continue
+            curr.remove(rmIndex)
+
+            if (curr.size < 4 && added.contains(adj).not()) {
+                queue.add(adj)
+                added.add(adj)
+            }
+
+            adjList[adj] = curr
+        }
+
+        adjList.remove(rmIndex)
+    }
+
+    println(count)
 }
