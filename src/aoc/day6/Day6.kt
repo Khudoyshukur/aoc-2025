f3bd5f745c5a44ef7504eb637bda4b182bbf6a48
kjuraev <kjuraev.001@gmail.com>
2025-12-06 11:15:14 +0500

    Complete day6

--- src/aoc/day6/Day6.kt
+++ src/aoc/day6/Day6.kt
@@ -6,9 +6,42 @@ import java.io.FileReader
 fun main() {
 
     val file = File("src/aoc/day6/input.txt")
-    file.createNewFile()
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
     val lines = input.split("\n")
+    println(lines)
+
+    val cols = hashMapOf<Int, MutableList<String>>()
+    val rowsCount = lines[0].split(" ")
+
+    for (i in lines.indices) {
+        val line = lines[i]
+
+        var currLine = line
+        while (currLine.contains("  ")) {
+            currLine = currLine.replace("  ", " ")
+        }
+        println(currLine)
+
+        val rows = currLine.split(" ").filter { it.isNotBlank() }
+        for (j in rows.indices) {
+            val curr = cols.getOrDefault(j, mutableListOf())
+            curr.add(rows[j])
+            cols[j] = curr
+        }
+    }
+
+    var res = 0L
+    for ((col, vals) in cols) {
+        if (vals.last() == "*") {
+            var curr = 1L
+            vals.dropLast(1).map { it.toLong() }.forEach { curr *= it }
+            res += curr
+        } else {
+            res += vals.dropLast(1).map { it.toLong() }.sum()
+        }
+    }
+
+    println(res)
 }
--- src/aoc/day6/Day6_2.kt
+++ src/aoc/day6/Day6_2.kt
@@ -3,13 +3,79 @@ package aoc.day6
 import java.io.File
 import java.io.FileReader
 
-
 fun main() {
 
     val file = File("src/aoc/day6/input.txt")
-    file.createNewFile()
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
     val lines = input.split("\n")
+
+    val iterators = MutableList(lines.size) { 0 }
+    var globalRes = 0L
+
+    while (true) {
+        while (true) {
+            for (index in 0 until iterators.size) {
+                if (iterators[index] < lines[index].length && lines[index][iterators[index]] == ' ') {
+                    iterators[index]++
+                }
+            }
+
+            var allDigit = true
+            for (index in iterators.indices) {
+                if (iterators[index] < lines[index].length && lines[index].getOrNull(iterators[index]) == ' ') {
+                    allDigit = false
+                }
+            }
+
+            if (allDigit) break
+        }
+
+
+        val ops = lines.last()[iterators[iterators.size - 1]]
+        println("$ops")
+        var res = if (ops == '*') 1L else 0L
+        val nums = mutableListOf<String>()
+
+        while (true) {
+            var hasDigit = false
+            for (index in iterators.dropLast(1).indices) {
+                if (lines[index].getOrNull(iterators[index])?.isDigit() == true) {
+                    hasDigit = true
+                    break
+                }
+            }
+            if (hasDigit.not()) {
+                iterators[iterators.lastIndex]++
+                break
+            }
+            //println("HasDigit: $hasDigit $iterators ${iterators.mapIndexed { index, value -> lines[index][value] }}")
+
+            val minIteratorIndex = iterators.dropLast(1).min()
+            val num = StringBuilder("")
+            for (index in iterators.indices) {
+                if (iterators[index] == minIteratorIndex) {
+                    if (lines[index].getOrNull(iterators[index])?.isDigit() == true) {
+                        num.append(lines[index][iterators[index]])
+                    }
+                    iterators[index]++
+                }
+            }
+            nums.add((num.toString()))
+        }
+        if (nums.isNotEmpty()) {
+            if (ops == '*') {
+                var curr = 1L
+                nums.map { it.toLong() }.forEach { curr *= it }
+                res = curr
+                //println("$nums -> $ops $res")
+            } else {
+                res += nums.sumOf { it.toLong() }
+            }
+            globalRes += res
+        }
+
+        println("$nums res: $res global: $globalRes op: $ops")
+    }
 }
