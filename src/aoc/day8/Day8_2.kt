a6dac3f31fb58095a8eb16608e3b55b449fe39cf
kjuraev <kjuraev.001@gmail.com>
2025-12-07 10:32:42 +0500

    Prepare day8

---
+++ src/aoc/day8/Day8_2.kt
@@ -0,0 +1,14 @@
+package aoc.day8
+
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+
+    val file = File("src/aoc/day8/input1.txt")
+    val fileReader = FileReader(file)
+
+    val input = fileReader.readText()
+    val lines = input.split("\n")
+
+}
