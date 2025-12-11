966e95eb6fbd46db210541cca45d4ed602fdcb85
kjuraev <kjuraev.001@gmail.com>
2025-12-11 10:30:17 +0500

    Complete day11

--- src/aoc/day11/Day11.kt
+++ src/aoc/day11/Day11.kt
@@ -5,10 +5,35 @@ import java.io.FileReader
 
 fun main() {
 
-    val file = File("src/aoc/day11/input1.txt")
+    val file = File("src/aoc/day11/input.txt")
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
     val lines = input.split("\n")
 
+    val adj = hashMapOf<String, MutableList<String>>()
+    for (line in lines) {
+        val root = line.substringBefore(":")
+        val paths = line.substringAfter(":").trim().split(" ")
+
+        adj[root] = (adj[root] ?: mutableListOf()).also {
+            it.addAll(paths)
+        }
+    }
+
+    fun paths(node: String, visit: MutableSet<String>): Long {
+        if (visit.contains(node)) return 0L
+        if (node == "out") return 1L
+        visit.add(node)
+
+        val res = adj[node].orEmpty().sumOf {
+            paths(it, visit)
+        }
+
+        visit.remove(node)
+
+        return res
+    }
+
+    println(paths("you", mutableSetOf()))
 }
--- src/aoc/day11/Day11_2.kt
+++ src/aoc/day11/Day11_2.kt
@@ -2,13 +2,54 @@ package aoc.day11
 
 import java.io.File
 import java.io.FileReader
+import java.util.LinkedList
+import java.util.Queue
 
 fun main() {
 
-    val file = File("src/aoc/day11/input1.txt")
+    val file = File("src/aoc/day11/input.txt")
     val fileReader = FileReader(file)
 
     val input = fileReader.readText()
     val lines = input.split("\n")
 
+    val adj = hashMapOf<String, MutableList<String>>()
+    for (line in lines) {
+        val root = line.substringBefore(":")
+        val paths = line.substringAfter(":").trim().split(" ")
+
+        adj[root] = (adj[root] ?: mutableListOf()).also {
+            it.addAll(paths)
+        }
+    }
+
+    val memo = hashMapOf<Triple<String, Boolean, Boolean>, Long>()
+    fun paths(node: String, dac: Boolean, fft: Boolean, visit: MutableSet<String>): Long {
+        val key = Triple(node, dac, fft)
+        if (memo.contains(key)) return memo[key]!!
+        if (visit.contains(node)) return 0L
+        if (node == "out") {
+            if (!dac || !fft) return 0L
+
+            return 1L
+        }
+        visit.add(node)
+
+        val res = adj[node].orEmpty().sumOf {
+            paths(
+                node = it,
+                dac = if (dac) true else (node == "dac"),
+                fft = if (fft) true else (node == "fft"),
+                visit = visit
+            )
+        }
+
+        visit.remove(node)
+
+        return res.also {
+            memo[key] = it
+        }
+    }
+
+    println(paths("svr", false, false, mutableSetOf()))
 }
