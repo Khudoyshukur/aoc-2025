87247facba7c295052615854534c341799a1f5c4
kjuraev <kjuraev.001@gmail.com>
2025-12-11 10:33:55 +0500

    Prepare day12

---
+++ src/aoc/day12/Day12_2.kt
@@ -0,0 +1,13 @@
+package aoc.day12
+
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+
+    val file = File("src/aoc/day11/input.txt")
+    val fileReader = FileReader(file)
+
+    val input = fileReader.readText()
+    val lines = input.split("\n")
+}
